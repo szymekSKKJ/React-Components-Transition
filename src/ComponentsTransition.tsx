bfbca92e8d836f65ffceabf53b3edd7d48556081
BuildTools <unconfigured@null.spigotmc.org>
2023-05-19 18:44:33 +0200

    Modyfied TransitionButton to behve like normall button with 'show' prop required

--- src/ComponentsTransition.tsx
+++ src/ComponentsTransition.tsx
@@ -2,32 +2,33 @@ import { Dispatch, ReactElement, SetStateAction, createContext, useContext, useS
 
 const CurrentVisibleComponentKeyContext = createContext<Dispatch<SetStateAction<string>> | null>(null);
 
-const TransitionButton = ({ ...props }) => {
+const TransitionButton = ({ show, children, ...props }: { show: string } & React.ComponentPropsWithoutRef<"button">) => {
   const setCurrentVisibleComponentKey = useContext(CurrentVisibleComponentKeyContext)!; // Not null because it is passing through context
 
   return (
     <button
       {...props}
-      onClick={() => {
-        setCurrentVisibleComponentKey(props.show);
+      onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        setCurrentVisibleComponentKey(show);
 
-        props.onClick && props.onClick();
+        props.onClick && props.onClick(e);
       }}>
-      {props.children}
+      {children}
     </button>
   );
 };
 
 export { TransitionButton };
 
-const ComponentsTransition = ({ children, visible }: { children: ReactElement[]; visible: string }) => {
-  const [currentVisibleComponentKey, setCurrentVisibleComponentKey] = useState<string>(visible);
+const ComponentsTransition = ({ children }: { children: ReactElement[] }) => {
+  const [currentVisibleComponentKey, setCurrentVisibleComponentKey] = useState<string>(children[0].key as string);
 
   return (
     <>
       <CurrentVisibleComponentKeyContext.Provider value={setCurrentVisibleComponentKey}>
         {children.map((child) => {
           const { key } = child;
+
           if (currentVisibleComponentKey === key) {
             return child;
           }
