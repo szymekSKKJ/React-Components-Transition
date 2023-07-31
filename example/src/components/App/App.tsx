6b213fc61709a23615ed1167cfb0d17bcecdb007
BuildTools <unconfigured@null.spigotmc.org>
2023-07-31 19:54:19 +0200

    Changed example and type fixed after build

--- example/src/components/App/App.tsx
+++ example/src/components/App/App.tsx
@@ -1,4 +1,3 @@
-import ComponentsTransition, { TransitionChildStatic } from "react-components-transition/ComponentsTransition";
 import "./App.css";
 import Exmaple1 from "./Exmaple1/Example1";
 import Exmaple2 from "./Exmaple2/Example2";
@@ -6,6 +5,7 @@ import Example3 from "./Example3/Example3";
 import Example4 from "./Example4/Example4";
 import ExampleMenu from "./ExampleMenu/ExampleMenu";
 import { useRef } from "react";
+import ComponentsTransition, { TransitionChildStatic } from "react-components-transition/ComponentsTransition";
 
 const App = () => {
   const exampleMenuRef = useRef(null);
@@ -16,14 +16,14 @@ const App = () => {
       <div className="example-menu" ref={exampleMenuRef}></div>
       <div className="content" ref={contentRef}>
         <ComponentsTransition parentElementRef={contentRef}>
-          <TransitionChildStatic renderToRef={exampleMenuRef}>
-            <ExampleMenu key="ExampleMenu"></ExampleMenu>
-          </TransitionChildStatic>
-
           <Exmaple1 key="Example1"></Exmaple1>
           <Exmaple2 key="Example2"></Exmaple2>
           <Example3 key="Example3"></Example3>
           <Example4 key="Example4"></Example4>
+
+          <TransitionChildStatic renderToRef={exampleMenuRef}>
+            <ExampleMenu key="ExampleMenu"></ExampleMenu>
+          </TransitionChildStatic>
         </ComponentsTransition>
       </div>
     </div>
